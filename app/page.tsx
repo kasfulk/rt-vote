dbd68687717092c3249e315b67381ea258b5d08e
Kasjful Kurniawan <kasful.bpp@gmail.com>
2025-11-30 17:53:30 +0800

    refactor(page): split heading into h1 and h2 for better semantic structure

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,8 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="text-center sm:text-left">
-          <h1 className="text-4xl font-bold mb-2">Pemilihan Ketua RT <br />RT. 61 Kel. Batu Ampar, Kec. Balikpapan Utara, Balikpapan - 2025</h1>
+          <h1 className="text-4xl font-bold mb-2">Pemilihan Ketua RT</h1>
+          <h2 className="text-2xl font-bold mb-2">RT. 61 Kel. Batu Ampar, Kec. Balikpapan Utara,<br />Balikpapan - 2025</h2>
           <p className="text-lg text-gray-600 dark:text-gray-400">Top 3 Kandidat dengan Suara Terbanyak</p>
         </div>
 
